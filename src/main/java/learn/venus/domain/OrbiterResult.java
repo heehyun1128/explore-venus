7da539905ecd84422f06ae46e01ee6dd02e68f73
Yi Chen <chenyiapp@yahoo.com>
2024-01-08 00:21:02 -0800

    complete test cases

--- src/main/java/learn/venus/domain/OrbiterResult.java
+++ src/main/java/learn/venus/domain/OrbiterResult.java
@@ -3,6 +3,7 @@ package learn.venus.domain;
 import learn.venus.models.Orbiter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class OrbiterResult  {
     private ArrayList<String> messages=new ArrayList<>();
@@ -22,5 +23,12 @@ public class OrbiterResult  {
 
     public void setPayload(Orbiter payload){
         this.payload=payload;
+    }
+
+    public List<String> getMessages() {
+        return new ArrayList<>(messages); //create a copy of the original list, preventing external code from modifying the internal list directly.
+//        The purpose of returning a copy is often to provide a read-only view of the error messages.
+//        This helps maintain encapsulation by not exposing the internal state of the class directly.
+//        External code can access the error messages without being able to modify the original list.
     }
 }
--- src/main/java/learn/venus/domain/OrbiterService.java
+++ src/main/java/learn/venus/domain/OrbiterService.java
@@ -7,5 +7,24 @@ public class OrbiterService {
 //    cant be null; name required; modules can hold 4 astronaut
 //    module with docker==4 astronaut, 1 shuttle
 
-    public add(Orbiter orbiter)
+    public OrbiterResult add(Orbiter orbiter){
+        OrbiterResult result=validateInputs(orbiter);
+        if(!result.isSuccess()){
+            return result;
+        }
+        return null;
+    }
+
+    private OrbiterResult validateInputs(Orbiter orbiter){
+        OrbiterResult result=new OrbiterResult();
+        if(orbiter==null){
+            result.addErrorMessage("Orbiter cannot be null");
+            return result;
+        }
+
+        if(orbiter.getName()==null || orbiter.getName().trim().length()==0){
+            result.addErrorMessage("Name is required.");
+        }
+        return result;
+    }
 }
